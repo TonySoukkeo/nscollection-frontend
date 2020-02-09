25fb4f4537b19acb57ccc0101c471f93a324b42b
Tony <tonysoukkeodesign@gmail.com>
2020-02-08 19:30:10 -0700

    Worked on home layout for mobile

--- src/layout/home/Home.js
+++ src/layout/home/Home.js
@@ -1,7 +1,23 @@
 import React from "react";
 
 const Home = () => {
-  return <section className="home">Home page</section>;
+  return (
+    <section className="home">
+      {/**** Game showcase ****/}
+      <div className="game-showcase">
+        <div className="game-showcase--box game-showcase__new">
+          New releases
+        </div>
+        <div className="game-showcase--box game-showcase__coming-soon">
+          Coming Soon
+        </div>
+        <div className="game-showcase--box game-showcase__sale">Sale</div>
+        <div className="game-showcase--box game-showcase__demo">Demos</div>
+
+        <div className="game-showcase--box game-showcase__all">All Games</div>
+      </div>
+    </section>
+  );
 };
 
 export default Home;
